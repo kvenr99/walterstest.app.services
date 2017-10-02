2264e9a1f7f17b6167475a5827949c7ca8c1b998
venkat kondabolu <kvenr99@gmail.com>
2017-10-02 01:24:12 -0500

    IOC settings

--- app.services/Services/Company/Company.API/App_Start/UnityConfig.cs
+++ app.services/Services/Company/Company.API/App_Start/UnityConfig.cs
@@ -1,20 +1,27 @@
 using Microsoft.Practices.Unity;
 using System.Web.Http;
 using Unity.WebApi;
-
+using Company.BL.Interfaces;
+using Company.BL;
 namespace Company.Api
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class UnityConfig
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public static void RegisterComponents()
         {
-			var container = new UnityContainer();
-            
+            var container = new UnityContainer();
+            container.RegisterType<IAccount, Account>();
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-            
+
             // e.g. container.RegisterType<ITestService, TestService>();
-            
+
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
